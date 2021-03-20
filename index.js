6e701f8c1045a67d1e59a7a5b2bd4e9a04a2a68b
samgozman <sam@gozman.space>
2021-03-20 12:20:31 +0300

    Add Financial annual page parser

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const quotes = require('./lib/quotes')
+const financials = require('./lib/financials')
 
 const main = async () => {
     console.log(await quotes.overview('AAPL'))
+    console.log(await financials.income().annual('AAPL'))
 }
 
 main()
