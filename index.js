3a8fe57766cc3204b2d8b386dd157453285c7c25
samgozman <sam@gozman.space>
2021-03-20 18:25:50 +0300

    Financials: cash-flow

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const quotes = require('./lib/quotes')
 const financials = require('./lib/financials')
 
 const main = async () => {
-    console.log(await quotes.overview('AAPL'))
-    console.log(await financials.income().annual('AAPL'))
-    console.log(await financials.income().quarterly('AAPL'))
+    // console.log(await quotes.overview('AAPL'))
+    // console.log(await financials.income().annual('AAPL'))
+    // console.log(await financials.income().quarterly('AAPL'))
+    // console.log(await financials.cashFlow().annual('AAPL'))
+    console.log(await financials.cashFlow().quarterly('AAPL'))
 }
 
 main()
