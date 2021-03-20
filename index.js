eab2fab8fd8daf26db60f02112f9513cee990026
samgozman <sam@gozman.space>
2021-03-20 13:56:31 +0300

    Add Financial quarterly page parser

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const financials = require('./lib/financials')
 const main = async () => {
     console.log(await quotes.overview('AAPL'))
     console.log(await financials.income().annual('AAPL'))
+    console.log(await financials.income().quarterly('AAPL'))
 }
 
 main()
