d7a1823e957bcd779cf29324f3c6194f73aeee53
samgozman <sam@gozman.space>
2021-03-22 12:02:41 +0300

    Add ESLint

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const quotes = require('./lib/quotes')
 const financials = require('./lib/financials')
 
-const main = async () => {
-    // console.log(await quotes.overview('AAPL'))
-    // console.log(await financials.income().annual('AAPL'))
-    // console.log(await financials.income().quarterly('AAPL'))
-    // console.log(await financials.cashFlow().annual('AAPL'))
-    // console.log(await financials.cashFlow().quarterly('AAPL'))
-    // console.log(await financials.balanceSheet().annual('AAPL'))
-    console.log(await financials.balanceSheet().quarterly('AAPL'))
-}
+// const main = async () => {
+//     console.log(await quotes.overview('AAPL'))
+//     console.log(await financials.income().annual('AAPL'))
+//     console.log(await financials.income().quarterly('AAPL'))
+//     console.log(await financials.cashFlow().annual('AAPL'))
+//     console.log(await financials.cashFlow().quarterly('AAPL'))
+//     console.log(await financials.balanceSheet().annual('AAPL'))
+//     console.log(await financials.balanceSheet().quarterly('AAPL'))
+// }
 
-main()
+// main()
+
+module.exports = {
+    quotes,
+    financials
+}
